e0102701dba7cc2435f466886811836ce45e0242
nmhung1210 <nmhung1210@gmail.com>
2019-01-01 17:24:20 +0700

    Update type

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,7 @@
-declare module 'remodux' {
-  export class Store {
-    constructor(reducer?: any, middlewares?: any[]);
-    reducers: any;
-    middlewares: any[];
-    useUndoRedo: boolean;
-    getState(): any;
-    dispatch(): any;
-    subscribe(listener: any): void;
-  }
+declare module 'react-remodux' {
+  export function connect(
+    store: any,
+    mapStateToProps?: (state: any) => any,
+    mapDispatchToProps?: (dispatch: any) => any
+  );
 }
